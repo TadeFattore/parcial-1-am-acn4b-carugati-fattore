bfee94dfbd4ad3d4471f4e55d0961d86a8680c72
tadef <tadef@DESKTOP-F3LITGC>
2023-10-07 00:08:55 -0300

    TF - Initial commit

---
+++ app/src/main/java/com/example/parcial_1_am_acn4b_carugati_fattore/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.parcial_1_am_acn4b_carugati_fattore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
