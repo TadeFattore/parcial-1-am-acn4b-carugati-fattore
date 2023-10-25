105a05eedbaeb53fb3c40b0afabdc33f42ceed46
scarugatidev <sebastian.carugati@davinci.edu.ar>
2023-10-25 13:37:37 -0300

    sc- Puse botones y acomode las medidas

--- app/src/main/java/com/example/parcial_1_am_acn4b_carugati_fattore/MainActivity.java
+++ app/src/main/java/com/example/parcial_1_am_acn4b_carugati_fattore/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.parcial_1_am_acn4b_carugati_fattore;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        Button button = findViewById();
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Botón presionado", Toast.LENGTH_SHORT).show();
+            }
+        });
+}}
