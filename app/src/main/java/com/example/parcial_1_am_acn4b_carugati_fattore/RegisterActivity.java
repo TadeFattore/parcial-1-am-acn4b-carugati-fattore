6981d34d17d50a0a840c88d058ef05d2ec6f86a7
tadef <tadef@DESKTOP-F3LITGC>
2023-12-18 21:36:48 -0300

    sc - Agregando diseño y lógica del Register

---
+++ app/src/main/java/com/example/parcial_1_am_acn4b_carugati_fattore/RegisterActivity.java
@@ -0,0 +1,68 @@
+package com.example.parcial_1_am_acn4b_carugati_fattore;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private EditText editTextName;
+    private EditText editTextLastName;
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        editTextName = findViewById(R.id.nombre);
+        editTextEmail = findViewById(R.id.correo);
+        editTextPassword = findViewById(R.id.contrasena);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+    }
+
+    public void onRegisterClick(View view) {
+        String name = editTextName.getText().toString();
+        String email = editTextEmail.getText().toString();
+        String password = editTextPassword.getText().toString();
+
+        if (name.isEmpty() || email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Ingrese todos los datos", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(RegisterActivity.this, "Registro exitoso", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(RegisterActivity.this, MainActivity.class));
+                            finish();
+                        } else {
+                            Toast.makeText(RegisterActivity.this, "Error al registrarse", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    public void onBackClick(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+}
